6db8f11e0a8fef10fa9b8f583750df9fc1532b9a
Lucas Rocha <lucasrocha.dv@gmail.com>
2020-11-07 09:38:40 -0300

    Typeorm's config

---
+++ ormconfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'type': 'sqlite',
+  'database': './src/database/db.sqlite',
+  'migrations': ['./src/database/migrations/*.ts'],
+  'entities': ['./src/models/*.ts'],
+  'cli': {
+    'migrationsDir': './src/dabatase/migrations'
+  }
+}
