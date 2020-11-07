41fc86ef2c7d5858509347235078208f01d00306
Lucas Rocha <lucasrocha.dv@gmail.com>
2020-11-07 12:09:55 -0300

    User's migration file

---
+++ src/database/migrations/1604758926107-create_users.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class createUsers1604758926107 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'users',
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          unsigned: true,
+          isPrimary: true,
+          isGenerated: true,
+          generationStrategy: 'increment'
+        },
+        {
+          name: 'email',
+          type: 'varchar'
+        },
+        {
+          name: 'password',
+          type: 'varchar'
+        },
+        {
+          name: 'reset_password_token',
+          type: 'varchar',
+          isNullable: true
+        },
+        {
+          name: 'reset_password_date_expires',
+          type: 'timestamp',
+          isNullable: true
+        }
+      ]
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+  }
+}
