0205b9bc99c94185089ed380db44989f3840289d
Lucas Rocha <lucasrocha.dv@gmail.com>
2020-11-07 11:21:47 -0300

    Update ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -4,6 +4,6 @@ module.exports = {
   'migrations': ['./src/database/migrations/*.ts'],
   'entities': ['./src/models/*.ts'],
   'cli': {
-    'migrationsDir': './src/dabatase/migrations'
+    'migrationsDir': 'src/database/migrations'
   }
 }
