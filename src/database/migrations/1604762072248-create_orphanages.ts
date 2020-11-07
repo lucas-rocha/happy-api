8769cf22db41ade14768259b9a8b0779883befd7
Lucas Rocha <lucasrocha.dv@gmail.com>
2020-11-07 12:21:26 -0300

    Orphanages' migration file

---
+++ src/database/migrations/1604762072248-create_orphanages.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class createOrphanages1604762072248 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'orphanages',
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          unsigned: true,
+          isPrimary: true,
+          isGenerated: true
+        },
+        {
+          name: 'latitude',
+          type: 'varchar'
+        },
+        {
+          name: 'longitude',
+          type: 'varchar'
+        },
+        {
+          name: 'about',
+          type: 'text'
+        },
+        {
+          name: 'instructions',
+          type: 'text'
+        },
+        {
+          name: 'opening_hours',
+          type: 'varchar'
+        },
+        {
+          name: 'open_on_weekends',
+          type: 'boolean',
+          default: false
+        },
+        {
+          name: 'is_check',
+          type: 'boolean',
+          default: false
+        }
+      ]
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orphanages')
+  }
+}
