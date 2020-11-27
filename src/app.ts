3e00e11705af2b325f663c8fc6b7083df7b2471c
Lucas Rocha <lucasrocha.dv@gmail.com>
2020-11-27 00:28:50 -0300

    Add Helmet

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
+import helmet from 'helmet'
 import cors from 'cors'
 import DBconnection from './database/connection'
 import routes from './routes'
 
 const app = express()
 
+app.use(helmet())
 app.use(cors())
 app.use(express.json())
 app.use(routes)
